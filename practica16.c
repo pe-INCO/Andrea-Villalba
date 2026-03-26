308dc9d8c16f81606b7496ee958ecf9b60c13e72
Andrea Villalba <andrea.villalba9442@alumnos.udg.mx>
2026-03-26 09:30:39 -0600

    Add files via upload

---
+++ practica16.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Andrea Villalba Mercado
+Programación estructurada
+26/03/2026
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int i, j, temp, n; 
+    int arreglo[6]={50,26,7,9,15,27};
+    n = sizeof(arreglo)/sizeof(arreglo[0]);
+        for(i=0; i<n-1; i++){
+            for(j=i+1;j<n; j++){
+                if(arreglo[i]>arreglo[j]){
+                temp=arreglo[i];
+                arreglo[i]=arreglo[j];
+                arreglo[j]=temp;
+            }
+        }
+    }
+    for(i=0; i<n-1; i++){
+        printf("%d, ", arreglo[i]);
+        
+    }
+    return 0;
+}
